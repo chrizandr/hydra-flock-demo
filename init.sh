7130ab13944edfb501e53736829df2e4418ed5e0
Chris Andrew <chris.g14@iiits.in>
2017-08-05 15:52:06 +0530

    Fixing setup.

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ echo "Starting Simulation Central Controller"
 python sim_controller/flock_controller/main.py &
 echo "Central Controller started successfully!"
 
-## Wait for 2 seconds.
-/bin/sleep 4
+## Wait for 8 seconds.
+/bin/sleep 8
 
 
 echo "Initializing Central Controller Location"
@@ -31,8 +31,8 @@ echo "Starting Simulation GUI"
 python sim_gui/app.py &
 echo "Simulation GUI started successfully"
 
-## Wait for 4 seconds.
-/bin/sleep 4
+## Wait for 8 seconds.
+/bin/sleep 8
 
 echo "Initializing Drone1"
 python sim_drone1/flock_drone/mechanics/drone_init.py
@@ -46,8 +46,8 @@ python sim_drone3/flock_drone/mechanics/drone_init.py
 echo "Initializing Drone4"
 python sim_drone4/flock_drone/mechanics/drone_init.py
 
-## Wait for 2 seconds.
-/bin/sleep 2
+## Wait for 8 seconds.
+/bin/sleep 8
 
 echo "Starting Drone1 main mechanics Loop"
 python sim_drone1/flock_drone/mechanics/listener.py &
